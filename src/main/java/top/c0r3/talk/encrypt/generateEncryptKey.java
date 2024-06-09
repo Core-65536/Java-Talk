8869728bae8a60656bf174571b6e0287bd3358f0
Core-65536 <bilibili.22@foxmail.com>
2024-06-09 11:22:34 +0800

    Ver 1.1 新增了AES加密,可以对发送的信息进行对称加密

--- src/main/java/top/c0r3/talk/encrypt/generateEncryptKey.java
+++ src/main/java/top/c0r3/talk/encrypt/generateEncryptKey.java
@@ -5,23 +5,23 @@ public class generateEncryptKey implements KeyGenerator{
     @Override
     public String generateKey(String seed) {
         Random rand = new Random(seed.hashCode());
-        String key = "";
+        StringBuilder key = new StringBuilder();
         for(int i = 0; i < 16; i++){
             switch (rand.nextInt(3)) {
                 case 0:
                     char c = (char) (rand.nextInt(26) + 65);
-                    key += c;
+                    key.append(c);
                     break;
                 case 1:
                     char d = (char) (rand.nextInt(26) + 97);
-                    key += d;
+                    key.append(d);
                     break;
                 case 2:
                     char e = (char) (rand.nextInt(10) + 48);
-                    key += e;
+                    key.append(e);
                     break;
             }
         }
-        return key;
+        return key.toString();
     }
 }
